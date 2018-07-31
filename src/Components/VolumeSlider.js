dc633ad4384311cc683b43d87a256de5f2821e37
TheNewStyles <tyler.peterson@utah.edu>
2018-07-31 06:59:07 -0500

    Build volum functionality

--- src/Components/VolumeSlider.js
+++ src/Components/VolumeSlider.js
@@ -9,14 +9,17 @@ export class VolumeSlider extends Component {
 
   handleChange(e) {
     e.preventDefault();
-    const drums = document.getElementsByClassName("drumpad");
-    drums.clips = 0.1 ? 0.99 : 0.1;
+    const volume = document.getElementById("volume").value / 100;
+    const drums = document.getElementsByClassName("clip");
+    for (let drum of drums) {
+      drum.volume = volume;
+    }
   }
 
   render() {
     return (
       <div>
-        <input type="range" min="0" max="100" step="1" defaultValue="1" onChange={this.handleChange}/>
+        <input id="volume" type="range" min="0" max="100" step="1" defaultValue="1" onChange={this.handleChange}/>
       </div>
     )
   }
