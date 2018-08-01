ca148b560f60d9519c6707d931635f2cae452c17
TheNewStyles <tyler.peterson@utah.edu>
2018-08-01 11:30:55 -0500

    Updates

--- src/__tests__/Drumpad.test.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import Drumpad from '../Components/Drumpad';
-
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<Drumpad />, div);
-  ReactDOM.unmountComponentAtNode(div);
-});
--- src/__tests__/VolumeSlider.test.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import VolumeSlider from '../Components/VolumeSlider';
-
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<VolumeSlider />, div);
-  ReactDOM.unmountComponentAtNode(div);
-});
